11d676a910bc694d9dbf5484e9ab8e810db0eb88
JoeBlack220 <348653205@qq.com>
2021-12-14 15:31:36 -0800

    sleep lab

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int n;
+  
+  if(argc < 2) {
+    fprintf(2, "Usage: sleep for x seconds...\n");
+    exit(1);
+  }
+
+  n = atoi(argv[1]);
+
+  sleep(n);
+  exit(0);
+}
