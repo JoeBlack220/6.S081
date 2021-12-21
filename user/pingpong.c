687bc3829131a1c731e433a5ad2b74668740b49b
JoeBlack220 <348653205@qq.com>
2021-12-20 21:02:30 -0800

    feat: finishi pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,40 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  // Ingnore any input arguments
+  
+  int parent[2];
+  int child[2];
+
+  // Create a pipe in p
+  pipe(parent);
+  pipe(child);
+  
+  char buf[1];
+  // child process, write one byte to child[1]
+  if(fork() == 0) {
+    read(parent[0], buf, 1);
+    if(buf[0] == 'p') {
+      printf("%d: received ping\n", getpid());
+    }
+    write(child[1], "c", 1);
+  } else { // parent process, write one byte to parent[1]
+    write(parent[1], "p", 1);
+    read(child[0], buf, 1);
+    if(buf[0] == 'c') {
+      printf("%d: received pong\n", getpid());
+    } 
+  }
+
+  // clean the pipes
+  close(parent[0]);
+  close(parent[1]);
+  close(child[0]);
+  close(child[1]);
+  exit(0);
+
+}
