c8aef3845ebe6d49731752bd495c4280b8902bf7
JoeBlack220 <348653205@qq.com>
2021-12-26 22:41:51 -0800

    feat: finish speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -105,6 +105,7 @@ static struct proc*
 allocproc(void)
 {
   struct proc *p;
+	struct usyscall u;
 
   for(p = proc; p < &proc[NPROC]; p++) {
     acquire(&p->lock);
@@ -119,14 +120,22 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
-
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
     freeproc(p);
     release(&p->lock);
     return 0;
   }
-
+	
+	// Allocate a usyscall page.
+	if((p->usyscall = (uint64)kalloc()) == 0) {
+		freeproc(p);
+		release(&p->lock);
+		return 0;
+	}
+	u.pid = p->pid;
+	*(struct usyscall *)p->usyscall = u;
+	
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,6 +162,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+	if(p->usyscall) 
+		kfree((void*)p->usyscall);
+	p->usyscall = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -195,7 +207,13 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
-
+	
+	// map the usyscall just below TRAPFRAME
+	if(mappages(pagetable, USYSCALL, PGSIZE, p->usyscall, PTE_R | PTE_W | PTE_U) < 0) {
+		uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+		uvmunmap(pagetable, TRAPFRAME, 1, 0);
+		uvmfree(pagetable, 0);
+	}
   return pagetable;
 }
 
@@ -206,6 +224,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+	uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
